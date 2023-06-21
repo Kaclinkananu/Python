9342ed6cd64d357f9c0b4aec46123b20a4fc59fb
Kaclinkananu <kananukaclin18@gmail.com>
2023-06-21 13:03:20 +0300

    My first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write A python program that prints the names of fruits.
+#Write a python program thats prints the names of cars.
+#Write a python program that prints the name of people.
+
+fruits = ["bananas","Apples","Mangoes","Oranges","Kiwi"]
+print(fruits)
+
+list1 = ["matatus","nganyas","nightnurse"]
+list2 = ["toyota","mitsubishi","subaru"]
+list3 = ["vitz","passo"]
+
+print("the following are cars I love",list1,list2)
+
+people = ["male","female","non-binary"]
+mwanaume,mwanamke,agender = people
+print(mwanaume)
+print(mwanamke)
+print(agender)
+
